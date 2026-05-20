679e1d8c67e5523224ed78d1b12826229ea321a8
rinamar123 <linsumarang151@gmail.com>
2026-05-19 22:45:49 -0500

    Fix DATABASE_URL handling: explicit check for empty env var

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -57,11 +57,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_project.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
