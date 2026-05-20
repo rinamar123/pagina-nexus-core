73b3617e4f4247c6f52e22547c3c828f5be49582
rinamar123 <linsumarang151@gmail.com>
2026-05-19 23:31:03 -0500

    Add datos.json fixture and seed_data command for production data load

--- enrollment/management/commands/seed_data.py
+++ enrollment/management/commands/seed_data.py
@@ -1,24 +1,11 @@
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Carga los datos iniciales de cursos, lecciones y exámenes'
+    help = 'Carga los datos desde datos.json'
 
     def handle(self, *args, **options):
-        self.stdout.write('Ejecutando populate_premium_full...')
-        exec(open('populate_premium_full.py').read())
-        self.stdout.write(self.style.SUCCESS('OK'))
-
-        self.stdout.write('Ejecutando setup_exam_questions...')
-        exec(open('setup_exam_questions.py').read())
-        self.stdout.write(self.style.SUCCESS('OK'))
-
-        self.stdout.write('Ejecutando final_content_setup...')
-        exec(open('final_content_setup.py').read())
-        self.stdout.write(self.style.SUCCESS('OK'))
-
-        self.stdout.write('Ejecutando populate_challenges...')
-        exec(open('populate_challenges.py').read())
-        self.stdout.write(self.style.SUCCESS('OK'))
-
-        self.stdout.write(self.style.SUCCESS('Todos los datos cargados exitosamente'))
+        self.stdout.write('Cargando datos desde datos.json...')
+        call_command('loaddata', 'datos.json')
+        self.stdout.write(self.style.SUCCESS('Datos cargados exitosamente'))
