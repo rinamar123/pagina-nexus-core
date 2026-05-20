5768b9c5607da69151e92ffd3c4927ebbef3ea60
rinamar123 <linsumarang151@gmail.com>
2026-05-20 01:23:05 -0500

    Deshabilita Gmail API, usa SMTP como unico canal

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -110,7 +110,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'sisxjpeqltgsytlk')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'IA & ML Academy <linsumarang151@gmail.com>')
 EMAIL_TIMEOUT = 30
 
-GMAIL_API_ENABLED = True
+GMAIL_API_ENABLED = False
 GMAIL_API_CREDENTIALS_FILE = BASE_DIR / 'client_secret_620518173132-qtioo9hdcvl506h4a0dopv53784ol298.apps.googleusercontent.com.json'
 GMAIL_API_TOKEN_FILE = BASE_DIR / 'token.json'
 GMAIL_API_SCOPES = ['https://www.googleapis.com/auth/gmail.send']
