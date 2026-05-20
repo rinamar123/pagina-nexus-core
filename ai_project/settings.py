c8b7ff0a341a08a513762a8ca874e7b60aecc6ae
rinamar123 <linsumarang151@gmail.com>
2026-05-19 22:36:28 -0500

    Fix deploy: remove migrate from build.sh, fix DATABASE_URL config, add worker-tmp-dir to Procfile

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'ai_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
     )
 }
 
